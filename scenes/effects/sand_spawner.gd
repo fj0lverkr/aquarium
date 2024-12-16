e217c3c833a2565f8eac80deb56400db35d1a4be
Nils Nahooy <nils.nahooy@live.com>
2024-12-16 20:37:59 +0100

    cull sand when amount reaches certain treshold

--- scenes/effects/sand_spawner.gd
+++ scenes/effects/sand_spawner.gd
@@ -3,10 +3,15 @@ extends Node2D
 
 const SAND: PackedScene = preload("res://scenes/effects/sand.tscn")
 
+@export
+var _max_sand: int = 1000
+
 var _sand_parent: Node
 var _enabled: bool = true # TODO: make this value depend on a game state indicating the player is in sand placement mode.
+var _sand_array: Array[Sand]
 
 func _ready() -> void:
+    _sand_array = []
     _sand_parent = get_tree().get_first_node_in_group(Constants.GRP_SAND)
 
 
@@ -20,6 +25,16 @@ func _spawn_sand() -> void:
     var s: Sand = SAND.instantiate()
     s.global_position = global_position
     _sand_parent.add_child(s)
+    _sand_array.append(s)
+    _cull_sand()
+
+
+func _cull_sand() -> void:
+    var sand_to_cull: int = _sand_array.size() - _max_sand
+    if sand_to_cull > 0:
+        for i: int in range(sand_to_cull):
+            var s: Sand = _sand_array.pop_at(0)
+            s.queue_free()
 
 
 func set_enabled(e: bool) -> void:
