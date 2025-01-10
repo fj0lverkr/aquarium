f8917f734485dfbf8c367646ca70f00fab50cbb3
Nils Nahooy <nils.nahooy@live.com>
2025-01-10 22:31:18 +0100

    spawn sand directly on servers, fine-tuning needed

--- scenes/effects/sand_spawner.gd
+++ scenes/effects/sand_spawner.gd
@@ -1,49 +1,65 @@
 class_name SandSpawner
 extends Node2D
 
-const SAND: PackedScene = preload("res://scenes/effects/sand.tscn")
-
 @export
 var _max_sand: int = 1500
+@export
+var _texture: Texture2D = preload("res://assets/images/effects/sand_8_8.png")
+
+var _canvas_item: RID
+var _rect: Rect2
+var _image: Image
+
+var _body: RID
+var _shape: RID
 
 var _sand_parent: Node
 var _enabled: bool = true # TODO: make this value depend on a game state indicating the player is in sand placement mode.
-var _sand_array: Array[Sand]
+var _sand_array: Array[RID]
+var _spawn_position: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
-    _sand_array = []
-    _sand_parent = get_tree().get_first_node_in_group(Constants.GRP_SAND)
+	_sand_array = []
+	_sand_parent = get_tree().get_first_node_in_group(Constants.GRP_SAND)
 
 
-func _process(_delta: float) -> void:
-    global_position = get_global_mouse_position()
-    if Input.is_action_pressed("LeftClick") and _enabled:
-        _spawn_sand()
-    if Input.is_action_just_released("LeftClick"):
-        await Util.wait(5)
-        call_deferred("_print_sand")
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("LeftClick") and _enabled:
+		_spawn_position = get_global_mouse_position()
+		_spawn_sand()
 
 
 func _spawn_sand() -> void:
-    var s: Sand = SAND.instantiate()
-    s.global_position = global_position
-    _sand_parent.add_child.call_deferred(s)
-    _sand_array.append(s)
-    #_cull_sand()
+	_setup_rendering()
+	_sand_array.append(_canvas_item)
+	_setup_physics()
 
 
-func _cull_sand() -> void:
-    var sand_to_cull: int = _sand_array.size() - _max_sand
-    if sand_to_cull > 0:
-        for i: int in range(sand_to_cull):
-            var s: Sand = _sand_array.pop_at(0)
-            s.queue_free()
+func _setup_rendering() -> void:
+	_image = _texture.get_image()
+	_canvas_item = RenderingServer.canvas_item_create()
+	_rect.size = Vector2(8, 8)
+	RenderingServer.canvas_item_set_parent(_canvas_item, get_canvas_item())
+	RenderingServer.canvas_item_add_texture_rect(_canvas_item, Rect2(-_texture.get_size() / 2, _texture.get_size()), _texture)
 
 
-func _print_sand():
-    var sleeping_sand: Array[Sand] = _sand_array.filter(func(s: Sand) -> bool: return s.sleeping)
-    var frozen_sand: Array[Sand] = _sand_array.filter(func(s: Sand) -> bool: return s.freeze)
-    print("%s sleeping, %s frozen, %s total" % [sleeping_sand.size(), frozen_sand.size(), _sand_array.size()])
+func _setup_physics() -> void:
+	_body = PhysicsServer2D.body_create()
+	PhysicsServer2D.body_set_mode(_body, PhysicsServer2D.BODY_MODE_RIGID)
+
+	_shape = PhysicsServer2D.circle_shape_create()
+	PhysicsServer2D.shape_set_data(_shape, 4)
+	PhysicsServer2D.body_add_shape(_body, _shape)
+	PhysicsServer2D.body_set_space(_body, get_world_2d().space)
+	PhysicsServer2D.body_set_state(_body, PhysicsServer2D.BODY_STATE_TRANSFORM, Transform2D(0, _spawn_position))
+	PhysicsServer2D.body_set_collision_layer(_body, 64) # layer 7
+	PhysicsServer2D.body_set_collision_mask(_body, 127) # mask 1-7
+	PhysicsServer2D.body_set_force_integration_callback(_body, _body_moved, _sand_array.size() - 1)
+
+
+func _body_moved(state: PhysicsDirectBodyState2D, index: int) -> void:
+	RenderingServer.canvas_item_set_transform(_sand_array[index], state.transform)
+
 
 func set_enabled(e: bool) -> void:
-    _enabled = e
+	_enabled = e
