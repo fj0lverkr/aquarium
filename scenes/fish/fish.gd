7cbceaeb6a4314883781748f3bb5debaf5633a17
Nils Nahooy <nils.nahooy@live.com>
2025-03-17 16:31:08 +0100

    setup idle state

--- scenes/fish/fish.gd
+++ scenes/fish/fish.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 ## Base class for Fish, all other Fish should inherit from this.
 
-enum State {IDLE, HUNT, REST, }
+enum State {IDLE, CHASING, RESTING, WANDERING, SEARCHING, FLEEING, }
 enum EmoteName {SLEEPING, }
 
 const EMOTES: Dictionary = {EmoteName.SLEEPING: "sleeping", }
@@ -65,13 +65,14 @@ var _current_state: State = State.IDLE
 var _prev_vel_x: float = 0.0
 var _distance_traveled: float = 0.0
 var _current_feed_target: Feed = null
+var _idle_tween: Tween
+var _is_idling: bool = false
 
 
 func _ready() -> void:
 	SignalBus.on_tank_changed.connect(_on_tank_changed)
 	for e: Sprite2D in _emotes.values():
 		e.hide()
-	set_physics_process(false)
 	if not _status_collection or _status_collection.get_collection().size() == 0:
 		print("Instance without StatusCollection removed.")
 		queue_free()
@@ -141,17 +142,16 @@ func _fish_look_at(where: Vector2) -> void:
 	var angle: float
 	var direction: Vector2
 
-	if _current_state != State.REST:
+	if _current_state == State.RESTING or _current_state == State.IDLE:
+		var tween: Tween = create_tween()
+		direction = Vector2.RIGHT if _prev_vel_x >= 0.0 else Vector2.LEFT
+		angle = (direction).angle()
+		tween.tween_property(self, "rotation", lerp_angle(rotation, angle, 1.0), ROTATION_TIME)
+		if direction == Vector2.LEFT:
+			_correct_orientation()
+	else:
 		direction = where
 		angle = (where - global_position).angle()
-	else:
-		direction = Vector2.RIGHT if _prev_vel_x > 0.0 else Vector2.LEFT
-		angle = (direction).angle()
-
-	if _current_state == State.REST:
-		var tween: Tween = create_tween()
-		tween.tween_property(self, "rotation", lerp_angle(rotation, angle, 1.0), ROTATION_TIME)
-	else:
 		look_at(direction)
 
 
@@ -182,23 +182,33 @@ func _get_corrected_scale(target: Vector2) -> Vector2:
 	return corrected_scale
 
 
-func _rest() -> void:
+func _idle_animation(is_resting: bool) -> void:
+	if _is_idling:
+		return
+	_is_idling = true
 	_fish_look_at(Vector2.ZERO)
 	await Util.wait(ROTATION_TIME)
-	_play_emote(EmoteName.SLEEPING)
 	ObjectFactory.spawn_mouth_bubbles(_mbe_marker.global_position, scale, _transient_children)
-	var tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BOUNCE)
-	tween.tween_property(self, "global_position:y", global_position.y - 1, 0.33)
-	tween.set_loops()
-	tween.tween_property(self, "global_position:y", global_position.y + 2, 0.33)
-	tween.tween_property(self, "global_position:y", global_position.y - 2, 0.33)
-	await Util.wait(randf_range(_wait_min_max.x, _wait_min_max.y))
+	_idle_tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BOUNCE)
+	_idle_tween.tween_property(self, "global_position:y", global_position.y - 1, randf_range(0.25, 0.35))
+	_idle_tween.set_loops()
+	_idle_tween.tween_property(self, "global_position:y", global_position.y + 2, randf_range(0.25, 0.35))
+	_idle_tween.tween_property(self, "global_position:y", global_position.y - 2, randf_range(0.25, 0.35))
+
+	if is_resting:
+		_play_emote(EmoteName.SLEEPING)
+		await Util.wait(randf_range(_wait_min_max.x, _wait_min_max.y))
+		_end_idle()
+		_stat_energy.increase(1000) # TODO make this depend on the time rested
+		_stop_emote(EmoteName.SLEEPING)
+
+
+func _end_idle() -> void:
 	for c: Node in _transient_children.get_children():
 		if c is MoutBubblesEmitter:
 			c.emitting = false
-	tween.kill()
-	_stat_energy.increase(1000) # TODO make this depend on the time rested
-	_stop_emote(EmoteName.SLEEPING)
+	_idle_tween.kill()
+	_is_idling = false
 
 
 func _play_emote(emote_name: EmoteName) -> void:
@@ -228,14 +238,18 @@ func _flip_debug_label(flip: bool) -> void:
 
 func _handle_sprite_animation() -> void:
 	match _current_state:
-		State.IDLE, State.HUNT:
+		State.CHASING, State.FLEEING:
 			if _anim_player.current_animation == SWIM and _anim_player.is_playing():
 				return
 			_anim_player.current_animation = SWIM
 			_anim_player.play()
-		State.REST:
+		State.SEARCHING, State.WANDERING:
+			# TODO setup slow swimming animation
+			pass
+		State.RESTING, State.IDLE:
 			if _anim_player.current_animation == SWIM and _anim_player.is_playing():
 				_anim_player.stop()
+			_idle_animation(_current_state == State.RESTING)
 
 
 func _calculate_resources_spent() -> void:
@@ -273,16 +287,16 @@ func _filter_feed_by_dl(f: Feed) -> bool:
 
 
 func _reset_feed_target() -> void:
-	if _current_state != State.HUNT:
+	if _current_state != State.CHASING:
 		return
 	_calculate_feed_target()
 
 
 func _reset_state() -> void:
 	if _stat_energy.get_stat_value() <= 0 and _stat_hunger.get_stat_value() > 0:
-		_current_state = State.REST
+		_current_state = State.RESTING
 	elif _stat_hunger.get_stat_value() <= 0:
-		_current_state = State.HUNT
+		_current_state = State.CHASING
 	else:
 		_current_state = State.IDLE
 
@@ -391,9 +405,9 @@ func _on_sv_depleted(s: StatusValue.StatusType) -> void:
 		StatusValue.StatusType.HEALTH:
 			pass
 		StatusValue.StatusType.ENERGY:
-			_current_state = State.REST
+			_current_state = State.RESTING
 		StatusValue.StatusType.HUNGER:
-			_current_state = State.HUNT
+			_current_state = State.CHASING
 
 
 func _on_sv_maxed_out(s: StatusValue.StatusType) -> void:
@@ -409,7 +423,7 @@ func _on_avoidance_area_body_entered(body: Node2D) -> void:
 		return
 
 func _on_avoidance_area_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	if area.get_parent() is Fish and _current_state != State.REST and _is_area_on_same_depth_layer(area):
+	if area.get_parent() is Fish and _current_state != State.RESTING and _is_area_on_same_depth_layer(area):
 		pass
 
 
@@ -422,7 +436,7 @@ func _on_mouth_area_body_entered(body: Node2D) -> void:
 		_stat_hunger.increase(f.nutri_value)
 		_stat_energy.increase(f.nutri_value * 0.5)
 		_stat_health.increase(f.nutri_value * 0.75)
-		if _current_state == State.HUNT:
+		if _current_state == State.CHASING:
 			_reset_state()
 		if _current_feed_target == f:
 			_current_feed_target = null
